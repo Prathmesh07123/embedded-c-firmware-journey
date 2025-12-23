bc917124c2b95904b7573ab654297fe4a78a36f1
Prathmesh07123 <pwani090@gmail.com>
2025-12-23 16:01:25 +0530

    Right rotation of array

---
+++ days/Day02_Array_Operations/Problem05_rotate_right.c
@@ -0,0 +1,35 @@
+/*
+ * Problem: Rotate array right by 1
+ *
+ * Firmware Usage:
+ * Used in circular buffers, time-shifted data,
+ * and sensor queue management
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define MAX_SIZE 10
+
+static uint16_t buffer[MAX_SIZE] = {10, 20, 30, 40, 50};
+static uint8_t length = 5;
+
+int main(void){
+
+    uint16_t temp = buffer[length - 1];
+
+    for(uint8_t i = length - 1; i > 0; i--){
+        buffer[i] = buffer[i - 1];
+    }
+
+    buffer[0] = temp;
+
+    printf("Rotated Right: ");
+
+    for (uint8_t i = 0; i < length; i++){
+        printf("%u ", buffer[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
